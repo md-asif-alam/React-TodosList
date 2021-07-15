08eddbf8f6036206aff220c8316251109b7cbd29
md-asif-alam <86280669+md-asif-alam@users.noreply.github.com>
2021-07-15 13:43:39 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an About component
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
+        perspiciatis exercitationem dolore blanditiis culpa eaque eligendi
+        tempore saepe odit, voluptatem facilis facere quis. Quo corporis facilis
+        temporibus cupiditate dolor illo alias non eaque aut!
+      </p>
+    </div>
+  );
+};
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">copyright &copy; MyTodosList.com</p>
+    </footer>
+  );
+};
